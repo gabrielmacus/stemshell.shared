aa4869e4733bd5316b48056629c4d89ff88bc8f4
Gabriel Macus <gabrielmacus@gmail.com>
2024-10-14 00:34:00 -0300

    deleted dtos

--- src/features/user/read-user.dto.ts
+++
@@ -1,5 +0,0 @@
-import { BaseModelDto } from "src/features/external/core/stemshell.api/src/features/crud/base-model.dto"
-
-export interface ReadUserDto extends BaseModelDto {
-    username:string
-}
